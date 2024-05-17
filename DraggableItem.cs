73bcd8d225a468a4d32945487e57c731bcd6ed2f
Yayayayayayyayayay <dora60819@gmail.com>
2024-05-17 21:22:21 +0800

    拖移物品加施放技能

---
+++ DraggableItem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    [HideInInspector] public Transform originalParent;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;  // 记录原始父对象
+        parentAfterDrag = originalParent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
