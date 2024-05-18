8913fb5e552ad63974bdc7b49e6e78f67a68deea
Joanne-0128 <forever125104@gmail.com>
2024-05-18 12:48:46 +0800

    新增程式碼
    AudioManager
    ChangeScence
    DonotDestroy
    StartScene

---
+++ ChangeScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagent;
+
+public class Scene : MonoBehaviour {
+    public string SceneName; // 目標場景的名稱
+
+    void Start() {
+        this.transform.tag = "";
+    }
+
+    public void ChangeScene() {
+        SceneManager.LoadScene(SceneName);
+    }
+}
