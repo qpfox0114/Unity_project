44919cd39393ede11cb06b3121c0bcf3275cab5d
Joanne-0128 <forever125104@gmail.com>
2024-05-21 14:54:14 +0800

    更新

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AudioManager : MonoBehaviour{
     public AudioClip bgm; // 背景音樂
     // 看那些功能需要音效可以再加
-    public AudioCLip seBubble; // 得到泡泡
+    public AudioClip seBubble; // 得到泡泡
     public AudioClip seBlock; // 放置區塊
     public AudioClip seClick; // 點選功能
     public AudioClip seJump; // 向上跳
@@ -16,17 +17,6 @@ public class AudioManager : MonoBehaviour{
         for (int i = 0; i < 6; i++){
             var audio = this.gameObject.AddComponent<AudioSource>(); // 在遊戲物件上添加 AudioSource 組件
             audios.Add(audio); // 將 AudioSource 加入清單中
-        }
-    }
-
-    // 使用的時候 GameManager.instance.audioManager.Play(index, name, true/false);
-    void Play(int index, string name, bool isloop) {
-        var clip = GetAudioClip(name);
-        if(clip != null){
-            var audio = audios[index];
-            audio.clip = clip;
-            audio.loop = isloop;
-            audio.Play();
         }
     }
 
@@ -48,4 +38,16 @@ public class AudioManager : MonoBehaviour{
         }
         return null;
     }
+
+    // 使用的時候 GameManager.instance.audioManager.Play(index, name, true/false);
+    void Play(int index, string name, bool isloop) {
+        var clip = GetAudioClip(name);
+        if(clip != null){
+            var audio = audios[index];
+            audio.clip = clip;
+            audio.loop = isloop;
+            audio.Play();
+        }
+    }
+
 }
