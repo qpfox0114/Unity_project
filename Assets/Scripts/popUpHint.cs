cf4831220b5a085f5edd9a2e18d2ba1e15c66928
ctrlz05 <a0966889598@gmail.com>
2024-05-23 17:27:55 +0800

    renew

---
+++ Assets/Scripts/popUpHint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class popUpHint : MonoBehaviour
+{
+    public GameObject hint;  // 参考提示窗口的Panel
+    public Button hintButton;     // 参考显示提示的按钮
+    public Button closeButton;    // 参考关闭提示的按钮
+
+    void Start()
+    {
+        // 初始化时隐藏提示窗口
+        hint.SetActive(false);
+
+        // 为按钮添加监听事件
+        hintButton.onClick.AddListener(ShowHint);
+        closeButton.onClick.AddListener(HideHint);
+    }
+
+    // 显示提示窗口
+    void ShowHint()
+    {
+        hint.SetActive(true);
+    }
+
+    // 隐藏提示窗口
+    void HideHint()
+    {
+        hint.SetActive(false);
+    }
+}
